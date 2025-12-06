cd8710d8fb0dac37c030674a9440389dab770639
Tali Gankin <tgankin@usc.edu>
2025-12-06 07:12:04 +0000

    finished wordle and sched

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,12 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool valid(const AvailabilityMatrix& avail, const DailySchedule& sched,
+            const vector<int>& worked, const size_t day,
+            const size_t maxShifts, const size_t worker);
+bool fillSched(const AvailabilityMatrix& avail, size_t dailyNeed,
+                size_t day, size_t shift, DailySchedule& sched, 
+                vector<int>& worked, const size_t maxShifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -31,15 +36,85 @@ bool schedule(
     const size_t maxShifts,
     DailySchedule& sched
 )
-{
+{   
+    //no days --> no sched needed
     if(avail.size() == 0U){
         return false;
     }
     sched.clear();
     // Add your code below
 
+    size_t workerCnt = avail[0].size();
+    size_t dayCnt = avail.size();
+
+    //track how many shfits each worker has been assigned
+    vector<int> shiftsWorked(workerCnt, 0);
+
+    //prepare output struct (1 empty vect/day)
+    sched = DailySchedule(dayCnt, vector<Worker_T>());
+
+    //start recursive backtrack 
+    return fillSched(avail, dailyNeed, 0, 0, sched, shiftsWorked, maxShifts);
 
 
 
 }
 
+bool fillSched(const AvailabilityMatrix& avail, size_t dailyNeed,
+                size_t day, size_t shift, DailySchedule& sched, 
+                vector<int>& worked, const size_t maxShifts)
+{
+    size_t nDays = avail.size();
+    size_t workerCnt = avail[0].size();
+
+    //base case: all days processed
+    if(day == nDays) return 1;
+
+    //filled all shifts for day? --> move to next day
+    if(sched[day].size() == dailyNeed){
+        return fillSched(avail, dailyNeed, day +1, 0, sched, worked, maxShifts);
+    }
+
+    //try all workers for next shift for current day
+    for(size_t worker = 0; worker < workerCnt; worker++){
+        if(valid(avail, sched, worked, day, maxShifts, worker)){
+            //temp assign this wrker
+            sched[day].push_back(worker);
+            worked[worker] ++;
+
+            //recurse assigning next shift/day
+            if(fillSched(avail, dailyNeed, day, shift +1, sched, worked, maxShifts)){
+                return 1; //found schedule
+            }
+            //backtrack (undo)
+            worked[worker] --;
+            sched[day].pop_back();
+            
+        }
+    }
+    //no worker can fill shift
+    return 0;
+}
+
+
+bool valid(const AvailabilityMatrix& avail, const DailySchedule& sched,
+            const vector<int>& worked, const size_t day,
+            const size_t maxShifts, const size_t worker)
+{
+    //available this day
+    if(!avail[day][worker]){
+        return 0;
+    }
+
+    //not exceeding max shifts
+    if((size_t)worked[worker] >= maxShifts){
+        return 0;
+    }
+
+    //check if not scheduled same day
+    if(find(sched[day].begin(), sched[day].end(), worker) != sched[day].end()){
+      return 0;
+    }
+
+    return 1;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -14,6 +14,19 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+//WORDLEHELPER WORKING TOO SLOW FOR LASR
+// void wordleHelper(const string& in,
+//                   string& current,
+//                   string floating,
+//                   const set<string>& dict,
+//                   set<string>& res,
+//                   size_t index);
+
+void genWords(string curr, string floating, int idx, 
+              const set<string>& dict, set<string>& res);
+int dashes(string in, int idx);
+
+
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +35,68 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> res;
+    genWords(in, floating, 0, dict, res);
+    return res;
+    // string current = in;
+    // wordleHelper(in, current, floating, dict, res, 0);
+    // return res;
 
 }
 
 // Define any helper functions here
+void genWords(string curr, string floating, int idx, 
+              const set<string>& dict, set<string>& res)
+{
+  //count num '-' pos starts at idx
+  size_t dashNum = dashes(curr, idx);
+
+  //pruning --> can't place all floating letters
+  if(floating.length() > dashNum) return;
+
+  //Base: no blanks left --> accept if all floats were used
+  if(dashNum == 0){
+    if(floating.length() == 0 && dict.find(curr) != dict.end()) res.insert(curr);
+    return;
+  }
+
+  //curr pos fixed --> move forward
+  if(curr[idx] != '-'){
+    genWords(curr, floating, idx+1, dict, res);
+  }
+  else{
+    //#1 --> try each float letter
+    for(size_t i = 0; i < floating.length(); i++){
+      char l = floating[i];
+
+      string next = curr;
+      next[idx] = l;
+
+      string new_float = floating;
+      new_float.erase(i, 1); //remove used floating l
+      genWords(next, new_float, idx + 1, dict, res);
+    }
+
+    //#2 --> try each reg letter
+    if(floating.length() < dashNum){
+      for(char l = 'a'; l <= 'z'; l++){
+        string next = curr;
+        next[idx] = l;
+        genWords(next, floating, idx+1, dict, res);
+      }
+    }
+  }
+}
+
+//cnt of how many '-' chars left from idx
+int dashes(string in, int idx)
+{
+  int count = 0;
+  for(size_t i = idx; i < in.length(); i++){
+    if(in[i] == '-'){
+      count++;
+    }
+  }
+  return count;
+}
+
